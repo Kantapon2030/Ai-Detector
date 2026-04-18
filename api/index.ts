24344fce58b7ddc1fd147bb2b0ebc74b432ab4f9
Kantapon2030 <tawna20081@gmail.com>
2026-04-19 00:33:49 +0700

    Fix Vercel serverless function handler

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,7 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../server';
-export default app;
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  // Use express as request handler for Vercel serverless
+  return app(req, res);
+}
